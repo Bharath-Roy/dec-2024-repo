b529ed83a061d17794fa76ea87a2b6d049df3950
sasidhar <janakisasidhar1@gmail.com>
2024-12-25 19:09:24 +0530

    added division

---
+++ division.py
@@ -0,0 +1,9 @@
+def division(x: int, y: int) -> int:
+    return x/y
+
+x = int(input('Enter x: '))
+y = int(input('Enter y: '))
+
+result = division(x, y)
+
+print(f'Division of {x} and {y} is {result}')
