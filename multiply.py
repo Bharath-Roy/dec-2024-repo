97b271208921c8036d826fdd97c3ca8e28956dc4
sasidhar <janakisasidhar1@gmail.com>
2024-12-25 18:54:10 +0530

    Added multiplication code

---
+++ multiply.py
@@ -0,0 +1,9 @@
+def multiply(x: int, y: int) -> int:
+    return x*y
+
+x = int(input('Enter x '))
+y = int(input('Enter y '))
+
+result = multiply(x,y)
+
+print(f'Multiplication result of {x} and {y} is {result}')
