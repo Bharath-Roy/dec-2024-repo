3ce455fd68daa098c29abd705000dd4d41f74320
sasidhar <janakisasidhar1@gmail.com>
2024-12-25 18:56:39 +0530

    subtraction-function-added

---
+++ subtraction.py
@@ -0,0 +1,9 @@
+def subtract(x: int, y: int) -> int:
+    return x-y
+
+x = int(input('Enter x: '))
+y = int(input('Enter y: '))
+
+result = subtract(x, y)
+
+print(f'Subtraction of {x} and {y} is {result}')
